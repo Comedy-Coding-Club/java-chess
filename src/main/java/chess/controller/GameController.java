0edd8a320cf5ba514c7a9faf23bd837833ea2448
zangsu <zangsu_@naver.com>
2024-03-25 12:46:00 +0900

    refactor (GameController) : ChessGame 객체를 필드가 아닌 지역변수로 가지도록 변경

--- src/main/java/chess/controller/GameController.java
+++ src/main/java/chess/controller/GameController.java
@@ -12,11 +12,14 @@ public class GameController {
     private static final InputView INPUT_VIEW = new InputView();
     private static final OutputView OUTPUT_VIEW = new OutputView();
 
-    private final Map<Command, Runnable> commandFunctions;
-    private ChessGame chessGame;
+    @FunctionalInterface
+    private interface GameStateChanger {
+        ChessGame change(ChessGame game);
+    }
+
+    private final Map<Command, GameStateChanger> commandFunctions;
 
     public GameController() {
-        chessGame = new InitialGame();
         commandFunctions = Map.of(
                 Command.START, this::startGame,
                 Command.MOVE, this::move,
@@ -27,36 +30,38 @@ public class GameController {
 
     public void run() {
         OUTPUT_VIEW.printGameStart();
-        runOrRetry();
+        ChessGame game = new InitialGame();
+        runOrRetry(game);
     }
 
-    private void runOrRetry() {
+    private void runOrRetry(ChessGame game) {
         try {
-            play();
+            play(game);
         } catch (IllegalArgumentException | IllegalStateException exception) {
             OUTPUT_VIEW.printExceptionMessage(exception.getMessage());
-            runOrRetry();
+            runOrRetry(game);
         } catch (RuntimeException exception) {
             OUTPUT_VIEW.printExceptionMessage("예기치 못한 동작입니다. 다시 명령어를 입력해 주세요.");
-            runOrRetry();
+            runOrRetry(game);
         }
     }
 
-    private void play() {
+    private void play(ChessGame chessGame) {
         while (chessGame.isNotEnd()) {
             Command command = INPUT_VIEW.readCommand();
-            Optional.ofNullable(commandFunctions.get(command))
+            chessGame = Optional.ofNullable(commandFunctions.get(command))
                     .orElseThrow(() -> new IllegalArgumentException("잘못된 커멘드 입력입니다."))
-                    .run();
+                    .change(chessGame);
         }
     }
 
-    private void startGame() {
+    private ChessGame startGame(ChessGame chessGame) {
         chessGame = chessGame.startGame(INPUT_VIEW::checkRestartGame);
         OUTPUT_VIEW.printBoard(chessGame.getBoard());
+        return chessGame;
     }
 
-    private void move() {
+    private ChessGame move(ChessGame chessGame) {
         String sourceInput = INPUT_VIEW.readLocation();
         String targetInput = INPUT_VIEW.readLocation();
 
@@ -65,9 +70,11 @@ public class GameController {
 
         chessGame.move(source, target);
         OUTPUT_VIEW.printBoard(chessGame.getBoard());
+
+        return chessGame;
     }
 
-    private void end() {
-        chessGame = chessGame.endGame();
+    private ChessGame end(ChessGame chessGame) {
+        return chessGame.endGame();
     }
 }
