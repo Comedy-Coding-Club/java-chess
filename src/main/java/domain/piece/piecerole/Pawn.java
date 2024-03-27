afdba6df04302eacde440a92e3f9d61e10b709ba
leetaehoon <xogns1514@gmail.com>
2024-03-27 20:49:25 +0900

    refactor(Pawn): 불필요한 메서드 제거

--- src/main/java/domain/piece/piecerole/Pawn.java
+++ src/main/java/domain/piece/piecerole/Pawn.java
@@ -1,10 +1,4 @@
 package domain.piece.piecerole;
 
-import domain.position.Position;
-
 public abstract class Pawn implements PieceRole {
-    @Override
-    public boolean canMove(final Position sourcePosition, final Position targetPosition) {
-        return true;
-    }
 }
