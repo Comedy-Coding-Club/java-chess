25ee888b5029ee4a56432490bac4fbe23d474bda
MingyeomKim <mingyum119@naver.com>
2024-03-31 17:43:39 +0900

    feat(GameSettingRepository): 기물을 옮기는 차례를 데이터베이스에서 변경하는 기능 구현

--- src/main/java/domain/game/Turn.java
+++ src/main/java/domain/game/Turn.java
@@ -4,6 +4,7 @@ import static domain.piece.Color.BLACK;
 
 import domain.piece.Color;
 import domain.piece.Piece;
+import java.util.Objects;
 
 public class Turn {
     private Color color;
@@ -29,5 +30,22 @@ public class Turn {
 
     public String getName() {
         return color.name();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Turn turn = (Turn) o;
+        return color == turn.color;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color);
     }
 }
--- src/test/java/repository/GameSettingsRepositoryTest.java
+++ src/test/java/repository/GameSettingsRepositoryTest.java
@@ -9,16 +9,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GameSettingsRepositoryTest {
-    private final GameSettingsRepository gameSettingsRepository = new GameSettingsRepository();
+    private final GameSettingRepository gameSettingRepository = new GameSettingRepository();
 
     @DisplayName("현재 차례의 정보를 저장한다.")
     @Test
     void saveTurn() {
         Turn turn = new Turn(Color.BLACK);
 
-        int rows = gameSettingsRepository.saveTurn(turn);
+        int rows = gameSettingRepository.saveTurn(turn);
 
         assertThat(rows).isEqualTo(1);
+    }
+
+    @DisplayName("현재 차례의 정보를 업데이트한다.")
+    @Test
+    void updateTurn() {
+        Turn first = new Turn(Color.WHITE);
+        Turn second = new Turn(Color.BLACK);
+        gameSettingRepository.saveTurn(first);
+
+        gameSettingRepository.updateTurn(second);
+
+        assertThat(gameSettingRepository.findTurn()).isEqualTo(second);
     }
 
     @DisplayName("게임 상태를 저장한다.")
@@ -26,7 +38,7 @@ class GameSettingsRepositoryTest {
     void saveGameState() {
         GameState gameState = GameState.STOPPED;
 
-        int rows = gameSettingsRepository.saveGameState(gameState);
+        int rows = gameSettingRepository.saveGameState(gameState);
 
         assertThat(rows).isEqualTo(1);
     }
