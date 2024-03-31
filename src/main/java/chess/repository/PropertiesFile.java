f48e9df4dd114ee5cf758f7afba7b50301529f18
zangsu <zangsu_@naver.com>
2024-03-31 15:22:54 +0900

    refactor(PropertiesFile) : 설정 파일의 1 depth 고정을 위해 yaml 대신 properties 파일 사용

--- src/main/java/chess/repository/PropertiesFile.java
+++ src/main/java/chess/repository/PropertiesFile.java
@@ -9,28 +9,30 @@ import java.util.Optional;
 import java.util.regex.Pattern;
 
 
-public class YmlFile {
-    private static final Pattern YML_PATTERN = Pattern.compile("(\\w+): (.+)"); //"[KEY]: [VALUE] (주석 포함 가능)"
+public class PropertiesFile {
+    private static final String DELIMITER = " = ";
+    private static final Pattern PROPERTY_PATTERN = Pattern.compile("(\\w.+)"+ DELIMITER + "(.+)");
+    // "[KEY] = [VALUE]"
 
     private final Map<String, String> properties;
 
-    public YmlFile(Map<String, String> properties) {
+    public PropertiesFile(Map<String, String> properties) {
         this.properties = properties;
     }
 
     /**
      *
-     * @param fileName src\main\resources 에 위치한 YML 파일
-     * @return 파일 이름으로 만들어진 YmlFile 클래스
+     * @param fileName src\main\resources 에 위치한 properties 파일
+     * @return 파일 이름으로 만들어진 PropertiesFile 클래스
      */
-    public static YmlFile of(String fileName) {
-        if (!fileName.contains("yml") && !fileName.contains("yaml")) {
-            throw new IllegalArgumentException("yaml 파일이 아닙니다.");
+    public static PropertiesFile of(String fileName) {
+        if (!fileName.contains("properties")) {
+            throw new IllegalArgumentException("properties 파일이 아닙니다.");
         }
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader("./src/\\main\\resources\\" + fileName));
             Map<String, String> properties = parsingProperty(bufferedReader);
-            return new YmlFile(properties);
+            return new PropertiesFile(properties);
         } catch (IOException e) {
             throw new RuntimeException("입출력 오류", e);
         }
@@ -48,12 +50,12 @@ public class YmlFile {
     }
 
     private static void saveProperty(String propertyLine, Map<String, String> properties) {
-        if (!YML_PATTERN.matcher(propertyLine).matches()) {
+        if (!PROPERTY_PATTERN.matcher(propertyLine).matches()) {
             return;
         }
-        String[] keyValue = propertyLine.split(": ");
+        String[] keyValue = propertyLine.split(DELIMITER);
         String key = keyValue[0];
-        String value = keyValue[1].split("#")[0].strip();
+        String value = keyValue[1].strip();
         properties.put(key, value);
     }
 
