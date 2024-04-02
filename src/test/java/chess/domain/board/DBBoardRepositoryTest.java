742ec1b039da06037dc6ed16b1e3cc832250b9ce
jhon3242 <rldotnr2@gmail.com>
2024-04-03 00:02:53 +0900

    refactor : 불필요한 메서드 삭제

--- src/test/java/chess/domain/board/DBBoardRepositoryTest.java
+++ src/test/java/chess/domain/board/DBBoardRepositoryTest.java
@@ -2,12 +2,12 @@ package chess.domain.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import chess.db.BoardDao;
 import chess.db.DBBoardRepository;
+import chess.db.DBConnectionUtils;
 import chess.domain.Color;
 import chess.domain.Piece;
 import chess.domain.PieceType;
-import chess.db.BoardDao;
-import chess.db.DBConnectionUtils;
 import chess.domain.position.Column;
 import chess.domain.position.Position;
 import chess.domain.position.Row;
@@ -82,58 +82,4 @@ class DBBoardRepositoryTest {
         //then
         assertThat(dbBoardRepository.hasPiece(position)).isFalse();
     }
-
-    /* 이건 Board 의 책임이 아닐까?
-    @DisplayName("해당 위치에 기물이 없으면 true 를 리턴한다.")
-    @Test
-    void isEmptySpaceTrueTest() {
-        //given
-        Position position = new Position(Row.RANK5, Column.C);
-
-        //when
-        chessBoard.initNewBoard(START_COLOR);
-
-        //then
-        assertThat(chessBoard.isEmptySpace(position)).isTrue();
-    }
-
-    @DisplayName("해당 위치에 기물이 있으면 False 를 리턴한다.")
-    @Test
-    void isEmptySpaceFalseTest() {
-        //given
-        Position position = new Position(Row.RANK1, Column.C);
-        Piece piece = new Piece(PieceType.ROOK, Color.WHITE);
-
-        //when
-        chessBoard.placePiece(position, piece);
-
-        //then
-        assertThat(chessBoard.isEmptySpace(position)).isFalse();
-    }
-
-    @DisplayName("보드판에 왕이 2명이 있으면 true 를 리턴한다")
-    @Test
-    void hasTowKingTrueTest() {
-        //when
-        chessBoard.initNewBoard(START_COLOR);
-
-        //then
-        assertThat(chessBoard.hasTwoKing()).isTrue();
-    }
-
-    @DisplayName("보드판에 왕이 2명이 있으면 false 를 리턴한다")
-    @Test
-    void hasTowKingFalseTest() {
-        //given
-        Piece whiteKing = new Piece(PieceType.KING, Color.WHITE);
-        Piece blackRook = new Piece(PieceType.ROOK, Color.BLACK);
-
-        //when
-        chessBoard.placePiece(new Position(Row.RANK1, Column.D), whiteKing);
-        chessBoard.placePiece(new Position(Row.RANK8, Column.D), blackRook);
-
-        //then
-        assertThat(chessBoard.hasTwoKing()).isFalse();
-    }
-    }*/
 }
