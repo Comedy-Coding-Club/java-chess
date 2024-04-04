919eb2522b5b5ceefeb52997f6c9c324e118230a
zangsu <zangsu_@naver.com>
2024-04-04 15:27:26 +0900

    refactor : 가독성을 위해 트랜잭션 단위의 로직들을 메서드로 추출

--- src/main/java/chess/service/GameService.java
+++ src/main/java/chess/service/GameService.java
@@ -53,28 +53,32 @@ public class GameService {
     }
 
     private Optional<Board> createBoard(Connection connection, int lastGameId) throws SQLException {
-        Map<Location, Integer> pieceLocations = boardDao.findBoardById(connection, lastGameId);
-        if (pieceLocations.isEmpty()) {
+        Map<Location, Integer> pieceIdsWithLocation = boardDao.findBoardById(connection, lastGameId);
+        if (pieceIdsWithLocation.isEmpty()) {
             return Optional.empty();
         }
-        Map<Location, Piece> board = new HashMap<>();
+        Board board = generateBoard(connection, pieceIdsWithLocation);
+        return Optional.of(board);
+    }
+
+    private Board generateBoard(Connection connection, Map<Location, Integer> pieceLocations)
+            throws SQLException {
+        Map<Location, Piece> piecesWithLocation = new HashMap<>();
         for (Entry<Location, Integer> locationIntegerEntry : pieceLocations.entrySet()) {
             Location location = locationIntegerEntry.getKey();
             Integer pieceId = locationIntegerEntry.getValue();
             Piece piece = pieceDao.findPieceById(connection, pieceId);
-            board.put(location, piece);
+            piecesWithLocation.put(location, piece);
         }
-        return Optional.of(new Board(board));
+        return new Board(piecesWithLocation);
     }
 
     public ChessGame createNewGame() {
         int lastGameId = transactionManager.getData(gameDao::findLastGameId);
         //TODO 방을 여러개 관리하게 되면 이전 데이터를 남겨두고 새로 만들어야 함
         transactionManager.executeTransaction(
-                connection -> {
-                    boardDao.deleteAllPiecesById(connection, lastGameId);
-                    gameDao.deleteGameById(connection, lastGameId);
-                });
+                connection -> deleteGame(connection, lastGameId)
+        );
 
         int newGameId = lastGameId;
         if (newGameId == 0) {
@@ -86,39 +90,43 @@ public class GameService {
     public ChessGame startGame(ChessGame chessGame, Supplier<Boolean> checkRestart) {
         ChessGame startedGame = chessGame.startGame(checkRestart);
         transactionManager.executeTransaction(
-                connection -> {
-                    gameDao.saveGame(connection, startedGame);
-                    this.saveAllPiece(connection, startedGame.getGameId(), startedGame.getBoard());
-                });
+                connection -> saveGame(connection, startedGame));
         return startedGame;
     }
 
-    private void saveAllPiece(Connection connection, int gameId, Board board) throws SQLException {
+    private void saveGame(Connection connection, ChessGame game) throws SQLException {
+        gameDao.saveGame(connection, game);
+        Board board = game.getBoard();
         for (Entry<Location, Piece> locationPieceEntry : board.getBoard().entrySet()) {
             Piece piece = locationPieceEntry.getValue();
             Location location = locationPieceEntry.getKey();
 
             int pieceId = pieceDao.getPieceId(connection, piece);
-            boardDao.savePieceLocation(connection, gameId, location, pieceId);
+            boardDao.savePieceLocation(connection, game.getGameId(), location, pieceId);
         }
     }
 
     public ChessGame move(ChessGame chessGame, Location source, Location target) {
         final var movedChessGame = chessGame.move(source, target);
         transactionManager.executeTransaction(
-                connection -> {
-                    boardDao.deletePieceLocation(connection, movedChessGame.getGameId(), target);
-                    boardDao.updatePieceLocation(connection, movedChessGame.getGameId(), source, target);
-                    gameDao.updateGame(connection, movedChessGame);
-                });
+                connection -> updateMovementData(connection, source, target, movedChessGame));
         if (movedChessGame.isEnd()) {
             transactionManager.executeTransaction(
-                    connection -> {
-                        boardDao.deleteAllPiecesById(connection, movedChessGame.getGameId());
-                        gameDao.deleteGameById(connection, movedChessGame.getGameId());
-                    });
+                    connection -> deleteGame(connection, movedChessGame.getGameId()));
         }
         return movedChessGame;
+    }
+
+    private void updateMovementData(Connection connection, Location source, Location target, ChessGame movedChessGame)
+            throws SQLException {
+        boardDao.deletePieceLocation(connection, movedChessGame.getGameId(), target);
+        boardDao.updatePieceLocation(connection, movedChessGame.getGameId(), source, target);
+        gameDao.updateGame(connection, movedChessGame);
+    }
+
+    private void deleteGame(Connection connection, int gameId) throws SQLException {
+        boardDao.deleteAllPiecesById(connection, gameId);
+        gameDao.deleteGameById(connection, gameId);
     }
 
     public ChessGame end(ChessGame chessGame) {
