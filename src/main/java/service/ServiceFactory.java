2c9d40b750a213a4d60c6d94c9001e0452e904c5
leetaehoon <xogns1514@gmail.com>
2024-04-04 17:50:24 +0900

    refactor(ServiceFactory): static scope에서 선언하도록 수정.

--- src/main/java/service/ServiceFactory.java
+++ src/main/java/service/ServiceFactory.java
@@ -7,13 +7,14 @@ import repository.dao.ChessGameDao;
 
 public class ServiceFactory {
     private static final ServiceFactory serviceFactory = new ServiceFactory();
-
-    private final ChessGameService chessGameService;
-
-    private ServiceFactory() {
+    private static final ChessGameService chessGameService;
+    static {
         ChessBoardRepositoryImpl chessBoardRepository = new ChessBoardRepositoryImpl(new ChessBoardDao());
         ChessGameRepositoryImpl chessGameRepository = new ChessGameRepositoryImpl(new ChessGameDao());
         chessGameService = new ChessGameService(chessBoardRepository, chessGameRepository);
+    }
+
+    private ServiceFactory() {
     }
 
     public static ServiceFactory getInstance() {
