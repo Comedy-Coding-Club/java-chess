2c5e748f438984df3bf8f6795a35a0d13ba965b6
leetaehoon <xogns1514@gmail.com>
2024-04-04 17:28:02 +0900

    feat(OutputView): continue option 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -11,6 +11,7 @@ public class OutputView {
     private static final OutputFormat FORMAT = new OutputFormat();
     private static final String COMMAND_MESSAGE = "> 체스 게임을 시작합니다.%n"
             + "> 게임 시작 : start%n"
+            + "> 게임 이어하기 : continue%n"
             + "> 게임 종료 : end%n"
             + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3%n"
             + "> 점수 계산 : status%n";
